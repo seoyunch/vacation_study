6d7de79b0ece26ab91263d88e14f486cd019a8ea
seoyunch <yen731013@naver.com>
2026-02-14 18:34:35 +0900

    [programmers] recommend new id

--- thrid_winter/NewId.java
+++ thrid_winter/NewId.java
@@ -1,6 +1,6 @@
 public class NewId {
     public static void main(String[] args) {
-        String new_id = "..123aaa~...deF.";
+        String new_id = "abcdefghijklmn.p";
         StringBuilder id = new StringBuilder(new_id.toLowerCase());//1단계
         int index = 0;
         while (index < id.length()) {//2단계
@@ -25,12 +25,36 @@ public class NewId {
                 index++;
             }
         }
-        while(String.valueOf(id.charAt(0)).equals(".") || String.valueOf(id.charAt(id.length()-1)).equals(".")){
+        //4단계
+        boolean end_is_point = true;
+        while(end_is_point == true){
+            if(id.length() == 0){
+                id.append("a"); // 5단계
+                break;
+            }
             if(String.valueOf(id.charAt(0)).equals(".")){
                 id.deleteCharAt(0);
-            }else if(String.valueOf(id.charAt(id.length()-1)).equals(".")){
-                id.deleteCharAt(id.length()-1);
+                continue;
+            }else{
+                if(String.valueOf(id.charAt(id.length()-1)).equals(".")){
+                    id.deleteCharAt(id.length()-1);
+                    continue;
+                }else{
+                    end_is_point = false;
+                    break;
+                }
             }
+        }
+        // 6단계
+        if(id.length() > 15){
+            id.delete(15, id.length());
+        }
+        if(String.valueOf(id.charAt(id.length()-1)).equals(".")){
+            id.deleteCharAt(id.length()-1);
+        }
+
+        while(id.length() < 3){
+            id.append(String.valueOf(id.charAt(id.length()-1)));
         }
         System.out.println(id);
     }
